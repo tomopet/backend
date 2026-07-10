4c41b45cd1aa1474da8351c5f0783d97c682c8ec
heeya1371 <rtg20051117@gmail.com>
2026-07-10 22:07:48 +0900

    Feat:create spring boot project

---
+++ src/main/java/com/nap/tomopet/TomopetApplication.java
@@ -0,0 +1,13 @@
+package com.nap.tomopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomopetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomopetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nap/tomopet/TomopetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nap.tomopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomopetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
