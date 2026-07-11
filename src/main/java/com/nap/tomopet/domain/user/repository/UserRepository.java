d4692ba352dea546577a2b683bbc862430894173
heeya1371 <rtg20051117@gmail.com>
2026-07-11 15:42:20 +0900

    Feat: create r

---
+++ src/main/java/com/nap/tomopet/domain/user/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.nap.tomopet.domain.user.repository;
+
+
+import com.nap.tomopet.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findbyEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+}
